609b03291b27756110273e8de9dcf4b2e09cfc16
Max Howell <mxcl@me.com>
2014-02-01 12:25:52 -0500

    v1

---
+++ README.md
@@ -0,0 +1,10 @@
+```ruby
+pod 'UIImage+AverageColor'
+```
+
+```objc
+#import "UIImage+AverageColor.h"
+
+UIImage *img = [UIImage imageNamed:@"foo"];
+UIColor *color = img.averageColor;
+```
---
+++ UIImage+AverageColor.m
@@ -0,0 +1,18 @@
+#import "UIImage+AverageColor.h"
+
+
+@implementation UIImage (AverageColor)
+
+- (UIColor *)averageColor {
+    CGSize size = {1, 1};
+    UIGraphicsBeginImageContext(size);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetInterpolationQuality(ctx, kCGInterpolationMedium);
+    [self drawInRect:(CGRect){.size = size} blendMode:kCGBlendModeCopy alpha:1];
+    uint8_t *data = CGBitmapContextGetData(ctx);
+    UIColor *color = [UIColor colorWithRed:data[2] / 255.f green:data[1] / 255.f blue:data[0] / 255.f alpha:1];
+    UIGraphicsEndImageContext();
+    return color;
+}
+
+@end
