5156367ea9896fbb4b5c9d57ad2f67fc722f0beb
Max Howell <mxcl@me.com>
2014-03-20 12:32:27 -0500

    Yeah. The other way around.

--- UIImage+AverageColor.m
+++ UIImage+AverageColor.m
@@ -10,7 +10,7 @@
     CGContextSetInterpolationQuality(ctx, kCGInterpolationMedium);
     [self drawInRect:(CGRect){.size = size} blendMode:kCGBlendModeCopy alpha:1];
     uint8_t *data = CGBitmapContextGetData(ctx);
-    UIColor *color = [UIColor colorWithRed:data[2] / 255.f green:data[1] / 255.f blue:data[0] / 255.f alpha:1];
+    UIColor *color = [UIColor colorWithRed:data[0] / 255.f green:data[1] / 255.f blue:data[2] / 255.f alpha:1];
     UIGraphicsEndImageContext();
     return color;
 }
